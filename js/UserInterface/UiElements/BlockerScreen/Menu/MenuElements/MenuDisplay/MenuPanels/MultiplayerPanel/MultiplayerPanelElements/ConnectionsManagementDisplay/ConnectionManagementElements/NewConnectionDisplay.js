4f8c6f2ec69416eec95e057c24174da732446abc
codeeMadness <codee.madness@gmail.com>
2022-06-13 13:41:31 +0700

    -- fix path

--- js/UserInterface/UiElements/BlockerScreen/Menu/MenuElements/MenuDisplay/MenuPanels/MultiplayerPanel/MultiplayerPanelElements/ConnectionsManagementDisplay/ConnectionManagementElements/NewConnectionDisplay.js
+++ js/UserInterface/UiElements/BlockerScreen/Menu/MenuElements/MenuDisplay/MenuPanels/MultiplayerPanel/MultiplayerPanelElements/ConnectionsManagementDisplay/ConnectionManagementElements/NewConnectionDisplay.js
@@ -1,4 +1,4 @@
-import { UiElement } from "../../../../../../../../../UiElement/UiElement.js";
+import { UiElement } from "../../../../../../../../../UiElement.js";
 class NewConnectionDisplay extends UiElement {
     constructor(){
         super({
